40a60eac3629eeefcad0d183be92860cf7231be8
aulouu <alina.minkovva@gmail.com>
2024-01-17 17:09:25 +0300

    checkboxx

--- src/main/webapp/resources/scripts/validX.js
+++ src/main/webapp/resources/scripts/validX.js
@@ -1,6 +1,6 @@
-function pressX(id){
+function pressX(id) {
     (x === parseInt(id.trim().substr(8))) ? document.getElementById(id).checked = true : x = parseInt(id.trim().substr(8));
-    for(let i=-3 ;i<=5; i++) if (id !== 'form:bcb'+i) document.getElementById('form:bcb'+i).checked = false;
+    for (let i = -5; i <= 1; i++) if (id !== 'mainForm:bcb' + i) document.getElementById('mainForm:bcb' + i).checked = false;
     console.log('X = ' + x);
-    document.getElementById('form:x').value = x;
+    document.getElementById('mainForm:x').value = x;
 }
