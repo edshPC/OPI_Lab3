818511708b6139f4ad85200fd4649ed85416ccc6
aulouu <alina.minkovva@gmail.com>
2024-02-13 22:43:31 +0300

    123

--- src/main/webapp/resources/scripts/validX.js
+++ src/main/webapp/resources/scripts/validX.js
@@ -1,13 +1,3 @@
-var x = 0;
-
-function pressX(id) {
-    console.log('x before: ' + x);
-    (x === parseInt(id.trim().substr(8))) ? document.getElementById(id).checked = true : x = parseInt(id.trim().substr(8));
-    for (let i = -5; i <= 1; i++) if (id !== 'mainForm:bcb' + i) document.getElementById('mainForm:bcb' + i).checked = false;
-    console.log('X = ' + x);
-    document.getElementById('mainForm:x').value = x;
-}
-
 function checkboxValidate(value) {
     let checkboxes = document.getElementsByClassName('checkbox');
     for (let checkbox of checkboxes) {
