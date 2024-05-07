b64654fb3439fe186181f02ceee467ca27ea30d3
aulouu <alina.minkovva@gmail.com>
2024-05-08 01:56:51 +0300

    add java tests

--- src/test/java/AreaCheckTest.java
+++ src/test/java/AreaCheckTest.java
@@ -11,10 +11,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AreaCheckTest {
     @Test
-    void one(){
-        assertTrue(AreaValidator.checkArea(new Point(2.0, (double) 0, 2.0)));
-        assertTrue(AreaValidator.checkArea(new Point(2.0, 1.0, 2.0)));
+    void checkAreaOnRectangle(){
+        assertTrue(AreaValidator.checkArea(new Point(-1.0, -2.0, 3.0)));
+        assertTrue(AreaValidator.checkArea(new Point(-2.0, -4.0, 5.0)));
+    }
+
+    @Test
+    void checkAreaOnTriangle(){
         assertTrue(AreaValidator.checkArea(new Point(1.0, 1.0, 2.0)));
-        assertTrue(AreaValidator.checkArea(new Point((double) 0, 1.0, 2.0)));
+        assertTrue(AreaValidator.checkArea(new Point(1.0, 4.0, 5.0)));
+    }
+
+    @Test
+    void checkAreaOnCircle(){
+        assertTrue(AreaValidator.checkArea(new Point(-1.0, 1.0, 2.0)));
+        assertTrue(AreaValidator.checkArea(new Point(-3.0, 0.5, 4.0)));
+    }
+
+    @Test
+    void checkAreaOnEmptyField(){
+        assertFalse(AreaValidator.checkArea(new Point(1.0, 2.0, 1.0)));
+        assertFalse(AreaValidator.checkArea(new Point(-1.0, 2.0, 2.0)));
+        assertFalse(AreaValidator.checkArea(new Point(-3.0, -1.0, 4.0)));
+        assertFalse(AreaValidator.checkArea(new Point(1.0, -1.5, 3.0)));
+    }
+
+    @Test
+    void checkAreaOnEmpty(){
+        assertTrue(AreaValidator.checkArea(new Point(0.0, 0.0, 2.0)));
     }
 }
