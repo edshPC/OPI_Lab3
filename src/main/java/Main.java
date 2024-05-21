dd559fa23ba466a652a97009810dcf394b71a60d
edshPC <ed_shh@mail.ru>
2024-05-21 18:27:17 +0300

    Revert "error test"
    This reverts commit c7433eb3364ebc3852007ec8caa46b6daa961c9e.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Error
         Scanner sc = new Scanner(System.in);
         var point = new Point();
         System.out.println("Enter point:");
